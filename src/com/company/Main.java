8102c83373d3cfce912a85ef342cb43c14fcc275
neriag <neriag@gmail.com>
2019-02-12 20:16:20 +0200

    initialize project

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
